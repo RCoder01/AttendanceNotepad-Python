26727c9484a3ab32b64eec59a8ad660b50a1fd0f
Arnav Mummineni <arnav.mummineni@gmail.com>
2021-05-28 00:53:07 -0400

    write_final outline and setup

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import csv
+from datetime import datetime
+import os
 
+months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+def get_next_ID():
+    return input('id:\n')
+
+def write_final(data):
+    for rel_path, ext in [('\\files\\logs', 'txt'), ('\\files\\tables', 'csv')]:
+        now = datetime.now()
+        out_dir = f'{os.getcwd()}{rel_path}\\{now.year}\\{months[now.month - 1]}'
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+
+        add = ['', 0]
+        while not os.path.isfile(f'{out_dir}\\{now.date}{add[0]}.{ext}'):
+            add[1] += 1
+            add[0] = ' (' + add[1] + ')'
+
+        if ext == 'txt':
+            continue
+        if ext == 'csv':
+            continue
+    return True
