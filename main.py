6da72cd00d2fc2ad6b6209b7d62dcc7607c7b8a4
Arnav Mummineni <arnav.mummineni@gmail.com>
2021-05-29 02:31:12 -0400

    Output CSV Reading and Formatting

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import os
 import numpy
 import pandas as pd
 
-
 months = [
     'January',
     'February',
@@ -21,8 +20,16 @@ months = [
     'December'
 ]
 
+
 def get_next_ID():
     return input('id:\n')
+
+def get_repeat_num(head, list):
+    add = ['', 0]
+    while head + add[0] in list:
+        add[1] += 1
+        add[0] = ' (' + str(add[1]) + ')'
+    return head + add[0]
 
 def write_final(data):
     for rel_path, ext in [('\\files\\logs', 'txt'), ('\\files\\tables', 'csv')]:
@@ -39,9 +46,9 @@ def write_final(data):
         if ext == 'txt':
             continue
         if ext == 'csv':
+            #out_df.reindex(sort_members(member_df).index)
             continue
     return True
-
 
 def sort_key(series):
     if series.name == 'Grade':
@@ -50,4 +57,45 @@ def sort_key(series):
     return string[:string.find(' ')] + string[string.find(' ') + 1:]
 
 def sort_members(df):
-    return df.sort_values(['Grade', 'Full Name'], key=sort_key)
+    try:
+        return df.sort_values(['Grade', 'Full Name'], key=sort_key)
+    except KeyError:
+        print('The "Member List" file was improperly formatted')
+
+def get_members():
+    try:
+        return pd.read_csv(os.getcwd() + '\\Member List.csv').set_index('ID')
+    except FileNotFoundError:
+        print('The "Member List" file has either been removed or renamed')
+        quit()
+
+def get_output_table():
+    file_path = os.getcwd() + '\\Output Table.csv'
+    if os.path.isfile(file_path):
+        try:
+            return pd.read_csv(file_path)
+        except pd.errors.EmptyDataError as err:
+            pass
+    open(file_path, 'w').close()
+    return get_members().drop(columns=['Grade'])
+
+def format_output_table(csv_table, member_table):
+    csv_table = pd.merge(
+        csv_table, 
+        member_table.drop(columns=['Grade']),
+        how='right', 
+        on=['ID', 'Full Name']
+    )
+    return csv_table.assign(
+        **{
+            get_repeat_num(
+                datetime.now().isoformat()[:10], 
+                csv_table.columns
+            ): [False for _ in range(len(csv_table))]
+        }
+    )
+
+if __name__ == '__main__':
+    mem = get_members()
+    out = get_output_table()
+    out2 = format_output_table(out, mem)
